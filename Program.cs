bf37fb4b2473aa4f4cfdc35ff778c6702157ef2d
Gabriel do nascimento carvalho <cintiaborba84@gmail.com>
2023-11-16 23:33:29 -0300

    Criando um método foreach

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace trabalhoFaculdade {
+    internal static class Program {
+
+        static void Main() {
+            Console.WriteLine("Precione ENTER para começar!");
+            Console.ReadLine();
+            exibirDias();
+        }
+
+        static void exibirDias() {
+            string[] diasArray = {"Segunda", "Terça","Quarta","Quita", "Sexta","Sábado", "Domingo"};
+
+            foreach(string dia in diasArray) {
+                Console.WriteLine($"Dia : {dia}");
+                Thread.Sleep(1500);
+            }
+
+        }
+
+    }
+}
